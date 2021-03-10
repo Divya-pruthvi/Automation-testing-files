fcde2ca33fc3fc466bfcc5aab7eeccea2216b559
Divya <Divya@DESKTOP-D2JD4LE>
2021-03-10 19:15:46 +0530

    interview programms

---
+++ javainterviewpreparation/src/javainterviewpreparation/Oddorevenfrom0to100.java
@@ -0,0 +1,20 @@
+package javainterviewpreparation;
+
+public class Oddorevenfrom0to100 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=100;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+				
+				
+				
+
+
+			
+		}
+		
+	}
+
+}
